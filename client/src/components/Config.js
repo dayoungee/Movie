c4619020f1405a214f37f4ff30cedf96291d083b
dayoungee <dbsek21@naver.com>
2022-04-07 21:54:00 +0900

    init

---
+++ client/src/components/Config.js
@@ -0,0 +1,8 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const API_KEY = 'ca0a10aea8713bc923cc3df945ea374c';
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
